594cc003ab7a021c5fcb1cfebfc9aa0d94e87a8e
fatetis <2580926706@qq.com>
2020-01-02 17:36:57 +0800

    调整baseService的定位-service层的全局方法

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -12,77 +12,6 @@ abstract class BaseService
     protected $table;
 
     /**
-     * 以id查询单条数据,查询失败报错
-     * @param $id array|string
-     * @return mixed
-     */
-    private function findOrFail($id)
-    {
-        return $this->table::findOrFail($id);
-    }
-    /**
-     * 以id查询单条数据
-     * @param $id array|string
-     * @return mixed
-     */
-    private function find($id)
-    {
-        return $this->table::find($id);
-    }
-
-    /**
-     * 以id查询单条数据,查询失败报错
-     * @param $id array|string
-     * @return mixed
-     */
-    private function firstOrFail($id)
-    {
-        return $this->table::firstOrFail($id);
-    }
-
-    /**
-     * 简单查询单条数据
-     * @param $where array
-     * @return mixed
-     */
-    private function first($where)
-    {
-        return $this->table::where($where)->first();
-    }
-
-    /**
-     * 插入数据
-     * @param $parameter array
-     * @return mixed
-     */
-    private function create($parameter)
-    {
-        return $this->table::create($parameter);
-    }
-
-    /**
-     * 条件插入记录，若无插入，有返回
-     * @param $where array
-     * @param $parameter array
-     * @return mixed
-     */
-    private function firstOrCreate($where, $parameter)
-    {
-        return $this->table::firstOrCreate($where, $parameter);
-    }
-
-    /**
-     * 条件插入记录，若无插入，有返回
-     * @param $where array
-     * @param $parameter array
-     * @return mixed
-     */
-    private function updateOrCreate($where, $parameter)
-    {
-        return $this->table::updateOrCreate($where, $parameter);
-    }
-
-    /**
      * 判断模型数据是否有效 依is_show来判断
      * @param $id string|array
      * @param $isAudit bool 可选是否加审核条件判断
@@ -103,56 +32,6 @@ abstract class BaseService
             return $result ? $result : false;
         }
 
-    }
-
-    private function get($where=[], $orderBy = [], $groupBy = [], $select = '')
-    {
-        $sql = $this->table::query();
-        if(!empty($where))
-            $sql->where($where);
-        if(!empty($orderBy)){
-            collect($orderBy)->filter(function($value,$key) use ($sql){
-                $sql->orderBy($key,$value);
-            });
-        }
-        if(!empty($groupBy)){
-            collect($groupBy)->filter(function($value,$key) use ($sql){
-                $sql->groupBy($key,$value);
-            });
-        }
-        if(!empty($select))
-            $sql->select($select);
-        return $sql->get();
-    }
-
-    private function pluck($key, $value)
-    {
-        return $this->query()->pluck($key, $value);
-    }
-
-    private function whereIn($key, $value)
-    {
-        return $this->query()->whereIn($key, $value);
-    }
-
-    private function where($where)
-    {
-        return $this->query()->where($where);
-    }
-
-    private function toArray()
-    {
-        return $this->table::toArray();
-    }
-
-    private function query()
-    {
-        return $this->table::query();
-    }
-
-    private function with($key, $fuc)
-    {
-        return $this->table::query()->with($key, $fuc);
     }
 
 
