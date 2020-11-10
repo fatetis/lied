382787f0fab28af6a057ac2efdcdcbaade08d20c
fatetis <2580926706@qq.com>
2020-11-10 14:56:41 +0800

    修改迁移文件的购物车表结构

--- database/migrations/2020_07_28_171139_create_cart_table.php
+++ database/migrations/2020_07_28_171139_create_cart_table.php
@@ -16,20 +16,17 @@ class CreateCartTable extends Migration
     {
         DB::statement("
 CREATE TABLE `lied_cart` (
-  `id` int(11) NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL COMMENT '用户id',
   `sku_id` int(11) unsigned NOT NULL COMMENT 'sku表主键id',
-  `brand_id` int(11) unsigned NOT NULL COMMENT '品牌id',
   `number` int(4) unsigned NOT NULL COMMENT '产品书数量',
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `cart-usre_id` (`user_id`),
-  KEY `cart-product_id` (`sku_id`),
-  KEY `cart-brand_id` (`brand_id`),
-  CONSTRAINT `cart-user_id` FOREIGN KEY (`user_id`) REFERENCES `lied_users` (`id`) ON UPDATE CASCADE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='购物车';
+
+
 
 ");
     }
